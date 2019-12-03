a26ff2bc286d165572b8362c2620e3ac54a578b1
Moshe Stern <moshecstern@gmail.com>
2019-12-03 14:17:08 -0500

    fixed error with heroku- put rating table took out of views. put back in when we have models

--- dbDONOTUSE/seeds.sql
+++ dbDONOTUSE/seeds.sql
@@ -1,15 +1,27 @@
-USE project2db;
-INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Double Donut Coffee","Bold", "NA");
-INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "West Coast Roast", "Bold", "Guy Fieri");
-INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "French Toast", "Sweet", "Java Factory Roasters");
-INSERT INTO Coffees (type, name, flavor, company, caf) VALUES("k-cups", "Coconut Caramel-icious", "Sweet", "Crazy Cups", false);
-INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Chocolate Marshmallow Swirl", "Sweet", "Friendlys");
-INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Mint Chocolate", "Sweet", "Andes");
-INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Dark Roast", "Bold", "Entenmanns");
-INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Rocky Road", "Sweet", "Sundae Coffee");
-INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Maple Sleigh", "Sweet", "Brooklyn Bean Roastery");
+-- USE project2db;
+-- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Double Donut Coffee","Bold", "NA");
+-- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "West Coast Roast", "Bold", "Guy Fieri");
+-- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "French Toast", "Sweet", "Java Factory Roasters");
+-- INSERT INTO Coffees (type, name, flavor, company, caf) VALUES("k-cups", "Coconut Caramel-icious", "Sweet", "Crazy Cups", false);
+-- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Chocolate Marshmallow Swirl", "Sweet", "Friendlys");
+-- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Mint Chocolate", "Sweet", "Andes");
+-- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Dark Roast", "Bold", "Entenmanns");
+-- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Rocky Road", "Sweet", "Sundae Coffee");
+-- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Maple Sleigh", "Sweet", "Brooklyn Bean Roastery");
 
 
 -- INSERT INTO user (username, rating, coffeeid) VALUES("Moshe Stern", 7, 1);
 
 -- INSERT INTO posts (title, message, userid) VALUES("Hello start", "lets see if this works", 1);
+
+
+USE project2db;
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Double Donut Coffee","Bold", "NA","/images/doubledonutbold.jpg");
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "West Coast Roast", "Bold", "Guy Fieri","/images/westcoastguyfieri.jpg");
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "French Toast", "Sweet", "Java Factory Roasters","/images/frenchtoastjava.jpg");
+INSERT INTO Coffees (type, name, flavor, company, caf, img) VALUES("k-cups", "Coconut Caramel-icious", "Sweet", "Crazy Cups", false,"/images/coconutcaramalcrazycups.jpg");
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Chocolate Marshmallow Swirl", "Sweet", "Friendlys","/images/chocmarshmellowswirlfriendlys.jpg");
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Mint Chocolate", "Sweet", "Andes","/images/andesmintchocochip.jpg");
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Dark Roast", "Bold", "Entenmanns","/images/entemensdarkroast.jpg");
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Rocky Road", "Sweet", "Sundae Coffee","/images/rockyroadsundae.jpg");
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Maple Sleigh", "Sweet", "Brooklyn Bean Roastery","/images/maplesleighbrooklyn.jpg");
