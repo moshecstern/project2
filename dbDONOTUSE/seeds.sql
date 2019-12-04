2b9b13ef0a35d83797bc765c4a5d5a9c3bd019d5
Moshe Stern <moshecstern@gmail.com>
2019-12-03 19:20:48 -0500

    added more to the database

--- dbDONOTUSE/seeds.sql
+++ dbDONOTUSE/seeds.sql
@@ -1,14 +1,4 @@
 -- USE project2db;
--- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Double Donut Coffee","Bold", "NA");
--- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "West Coast Roast", "Bold", "Guy Fieri");
--- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "French Toast", "Sweet", "Java Factory Roasters");
--- INSERT INTO Coffees (type, name, flavor, company, caf) VALUES("k-cups", "Coconut Caramel-icious", "Sweet", "Crazy Cups", false);
--- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Chocolate Marshmallow Swirl", "Sweet", "Friendlys");
--- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Mint Chocolate", "Sweet", "Andes");
--- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Dark Roast", "Bold", "Entenmanns");
--- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Rocky Road", "Sweet", "Sundae Coffee");
--- INSERT INTO Coffees (type, name, flavor, company) VALUES("k-cups", "Maple Sleigh", "Sweet", "Brooklyn Bean Roastery");
-
 
 -- INSERT INTO user (username, rating, coffeeid) VALUES("Moshe Stern", 7, 1);
 
@@ -25,3 +15,27 @@ INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Mint Ch
 INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Dark Roast", "Bold", "Entenmanns","/images/entemensdarkroast.jpg");
 INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Rocky Road", "Sweet", "Sundae Coffee","/images/rockyroadsundae.jpg");
 INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Maple Sleigh", "Sweet", "Brooklyn Bean Roastery","/images/maplesleighbrooklyn.jpg");
+
+-- rating: 8   Caramal Swirl, Sweet, Freindlys, caramalswirlfreindlys.jpg
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Caramal Swirl", "Sweet", "Freindlys","/images/caramalswirlfreindlys.jpg");
+-- rating: 8   Dark Roast, Bold, Entenmanns, darkroastentenmanns.jpg
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Dark Roast", "Bold", "Entenmanns","/images/darkroastentenmanns.jpg");
+-- rating: 9   Toasted Cinnamon, Sweet, Hamilton Mills, toastedcinnamonhamiltonmills.jpg
+INSERT INTO  Coffees (type, name, flavor, company, img) VALUES("k-cups", "Toasted Cinnamon", "Sweet", "Hamilton Mills","/images/toastedcinnamonhamiltonmills.jpg");
+-- rating: 8   French Roast, Bold, Van Houtte, frenchroastvanhoutte.jpg
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "French Roast", "Bold", "Van Houtte","/images/frenchroastvanhoutte.jpg");
+-- rating 7   Brooklyn Cherry Cheesecake, Sweet, Brooklyn Bean Roastery, brooklyncherrycheesecake.jpg
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Brooklyn Cherry Cheesecake", "Sweet", "Brooklyn Bean Roastery","/images/maplesleighbrooklyn.jpg");
+-- rating 8   Vanilla Swirl, Sweet, Sundae Coffee, vanillaswirlsundae.jpg
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Vanilla Swirl", "Sweet", "Sundae Coffee","/images/vanillaswirlsundae.jpg");
+--  rating 8    Original Dark, Dark Roast, Donut Shop, originaldarkdonutshop.jpg
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Original Dark", "Dark Roast", "Donut Shop","/images/originaldarkdonutshop.jpg");
+--  rating 9    Mint Chocolate Chip, Sweet, Sundae Coffee, mintchocolatechipsundae.jpg
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Mint Chocolate Chip", "Sweet", "Sundae Coffee","/images/mintchocolatechipsundae.jpg");
+--  rating 9   Coney Island Caramel, Sweet, Brooklyn Bean Roastery, coneyislandcaramelbrooklyn.jpg
+INSERT INTO Coffees (type, name, flavor, company, img) VALUES("k-cups", "Coney Island Caramel", "Sweet", "Brooklyn Bean Roastery","/images/coneyislandcaramelbrooklyn.jpg");
+
+
+
+
+-- maybe 3 options for rating: dislike 0, like 1, favs 2
